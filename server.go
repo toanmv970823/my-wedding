1e05900acbac08aad29113bb9443e9f0c74da742
toanmv0823 <toanmv@dlsinc.com>
2023-03-29 11:13:04 +0700

    Init project

---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    port := os.Getenv("PORT")
+
+    if port == "" {
+        port = "8000"
+    }
+
+    r := gin.Default()
+    
+    r.Static("/static", "resources/static")
+    r.LoadHTMLGlob("resources/*.html")
+
+    r.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.html", gin.H{
+            "title": "Home Page",
+        })
+    })
+
+    r.Run(fmt.Sprintf(":%s", port))
+}
